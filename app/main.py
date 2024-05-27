ad2f05d7ae1ab16dee9cacbdbfe2721bab97fde5
Eniiyi Bishop Ajala <eniiyibishopajala1@gmail.com>
2024-05-27 17:25:18 +0100

    continuation

--- app/main.py
+++ app/main.py
@@ -9,8 +9,8 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    # server_socket.accept()  # wait for client
-    # server_socket.accept()[0].sendall(b"HTTP/1.1 200 OK \r\n\r\n")  # wait for client
+    server_socket.accept()  # wait for client
+    server_socket.accept()[0].sendall(b"HTTP/1.1 200 OK \r\n\r\n")  # wait for client
 
     
 if __name__ == "__main__":
