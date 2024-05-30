d2e7886aaf19a65fe509b80f1e62c3d2f70f712a
Eniiyi Bishop Ajala <adedamolaajala33@gmail.com>
2024-05-30 09:50:57 +0100

    Adding Concurrency

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 import socket
 import re
+import threading
 
 def main():
     
@@ -9,73 +10,62 @@ def main():
     print("Server started, waiting for connections...")
 
     client, address = server_socket.accept()  # wait for client
-    print(f"""{client, address}
+    print(f"""The Client: {client}, The Client Address: {address}
           Received connection from: {address[0]}, port: {address[1]}""")
-
+    
+    headers = {}
+    response_body = ""
     with client:
+        # receving request from the client side using "recv()" method on the client and "decode()" method to decode from bytes to string.
         # using 4096 as buffer size to provide good performance and simple HTTP handling requests.
-        request = client.recv(4096).decode()
-        print(f"This is the request {request}")
+        client_request: str = client.recv(4096).decode()
+        print(f"This is the request {client_request}")
 
         # splitting the request
-        split_request = request.split("\r\n")
-        print(f"This is the Split Request {split_request}")
+        split_client_request: list[str] = client_request.split("\r\n")
+        print(f"This is the Split Request {split_client_request}")
 
         # should expect three set of split request GET /path HTTP/1.1
-        method, path, version = split_request[0].split(" ")
-        print(f"This is the method: ({method}), This is the path: ({path}), This is version: ({version})")
+        method, path, version = split_client_request[0].split(" ")
+        print(f"The Method: ({method}), The Path: ({path}), The Version: ({version})")
 
         if path == "/":
-            confirm_response = "HTTP/1.1 200 OK\r\n\r\n"
-            client.send(confirm_response.encode())
+            # using the "encode()" method to convert string to bytes, because the server reads bytes not strings."
+            confirm_response: bytes = "HTTP/1.1 200 OK\r\n\r\n".encode()
+            client.send(confirm_response)
 
         elif path == "/user-agent":
-            user_agent = next((user.split(":")[1] for user in split_request if user.startswith("User-Agent")),None)
+            # splitting the client data into a list of strings to give a list where each element is a line from the HTTP request
+            user_agent = next((client_data.split(":")[1] for client_data in split_client_request if client_data.startswith("User-Agent")),None)
             response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(user_agent)}\r\n\r\n{user_agent}\r\n"
+            # using the "encode()" method to convert string to bytes, because the server reads bytes not strings."
             client.send(response.encode())
+
+            headers["Content-Type"] = "text/plain"
+            headers["Content-Length"] = len(response_body)
 
         elif path.startswith("/echo"):
             random_path = path[6:]
             response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(random_path)}\r\n\r\n{random_path}\r\n"
+
+            # using the "encode()" method to convert string to bytes, because the server reads bytes not strings."
             client.send(response.encode())
+
+            headers["Content-Type"] = "text/plain"
+            headers["Content-Length"] = len(response_body)
 
         else:
             not_found_response = "HTTP/1.1 404 Not Found\r\n\r\n"
+
+            # using the "encode()" method to convert string to bytes, because the server reads bytes not strings."
             client.send(not_found_response.encode())
+            print("Error 404: Bad Request")
 
 
         client.close()
-
-        
-    # # receving data from the client side using "recv()" method on the client and "decode() method to decode from bytes to string"
-    # cli_data: str = client.recv(1024).decode()
-    # print("Decoding client data...")
-
-    # # splitting the client data into a list of strings to give a list where each element is a line from the HTTP request
-    # split_cli_data: list[str] = cli_data.split("\r\n")
-    # print("Splitting client data...")
+        print("Connection Closed")
+        server_socket.close()
 
 
-    # # response from the server
-    # response: bytes = "HTTP/1.1 200 OK\r\n\r\n".encode() 
-    # # using the "encode()" method to convert string to bytes, because the server reads bytes not strings."
-    # print("""Encoding Data...
-    #       Extraction Complete.""")
-
-    # # if split client data separated by space indexing the second element is not equal to "/"
-    # if split_cli_data[0].split(" ")[1] != "/":
-        # # response from the server
-    #     response = "HTTP/1.1 404 Not Found\r\n\r\n".encode()
-    #     print("Error 404: Bad Request")
-    # client.send(response)
-
-    # print("Disconnected from server.")
-    # client.close()
-    # server_socket.close()
-
-    # # print(f"Connected, client address: {address}")
-    # # client.sendall(b"HTTP/1.1 200 OK\r\n\r\n")  # wait for client
-
-    
 if __name__ == "__main__":
     main()
